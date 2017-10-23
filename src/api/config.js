86db17f90b29143b040851bc42decca8f02cb951
Diego Moreno <diegomoreno@cyandark.com>
2017-10-23 04:14:22 -0500

    Commit Inicial

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+	appName: 'platzimusic',
+	apiKey: '475c800879c2f4a634518ef9b22dcfd4',
+	secret: '351d8e40e64621e35934b73619645382',
+	registeredTo: 'TxusBlack'
+}
+
+
+// Application name	platzimusic
+// API key	475c800879c2f4a634518ef9b22dcfd4
+// Shared secret	351d8e40e64621e35934b73619645382
+// Registered to	TxusBlack
